5e3e9fb27f610795c5457c4401de4031ade071a1
ayush32121321-ui <ayush32121321@gmail.com>
2026-07-13 11:47:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -546,6 +546,7 @@ async def close_ticket(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def groupid(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(str(update.effective_chat.id))
 async def tag_users(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("TAG COMMAND WORKING")
 
     if not await is_admin(update, context):
         await update.message.reply_text("❌ Permission denied")
